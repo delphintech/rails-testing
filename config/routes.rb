e2390fb9d29862a166aad2c2cc8e8f8429f0055e
delphine abouab <delphine.abouab@outlook.fr>
2023-05-22 14:44:39 +0200

    initializing boiler plate for TDD

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  get 'products/index'
   devise_for :users
-  root to: "pages#home"
+  root to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
